c2be98df9a2450d65c2a63162c121d0d2837fa6f
EufranioDiogo <eufraniodiogo5@gmail.com>
2021-12-14 18:35:10 +0100

    :star: initial commit :star:

---
+++ dist/app.js
@@ -0,0 +1,14 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+var express_1 = __importDefault(require("express"));
+var app = (0, express_1.default)();
+var port = 3000;
+app.get('/', function (req, res) {
+    res.send('The sedulous hyena ate the antelope!');
+});
+app.listen(port, function () {
+    console.log('Servidor rodando');
+});
