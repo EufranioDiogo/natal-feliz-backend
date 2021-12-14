dc10ec5a98dd88ca75890d94c85893590042850d
EufranioDiogo <eufraniodiogo5@gmail.com>
2021-12-14 23:40:42 +0100

    feat: auth feature done

--- src/controllers/auth/authController.ts
+++ src/controllers/auth/authController.ts
@@ -1,6 +1,9 @@
-import { verifyUserByToken } from './../../helpers/security/jwt/jwtHelper';
-import {Request, Response} from 'express'
+import { compareEncryptedStringToNormal } from './../../helpers/security/encryption/encryptHelper';
+import { userModel } from './../../models/hiddenFriend/hiddenFriendModel';
+import { verifyUserByToken, generateJWT } from './../../helpers/security/jwt/jwtHelper';
+import { Request, Response } from 'express'
 import { responseGenerator } from '../../helpers/remote/response/responseGenerator';
+import { UserType } from '../../utils/vars/user/userVars';
 
 export const isAuthenticatedUser = (req: Request, res: Response) => {
   const user = verifyUserByToken(req.body?.token)
@@ -26,10 +29,48 @@ export const isAuthenticatedUser = (req: Request, res: Response) => {
   }
 }
 
-export const loginUser = (req: Request, res: Response) => {
+export const loginUser = async (req: Request, res: Response) => {
+  const user: UserType = {
+    username: req.body?.username,
+    password: req.body?.password,
+  }
 
+  const userFounded = await userModel.findOne({ username: user.username })
+
+  if (compareEncryptedStringToNormal(userFounded?.password, user?.password)) {
+    const token = generateJWT({
+      _id: userFounded?._id,
+      ...user
+    })
+
+    responseGenerator(res, {
+      url: req.url,
+      status: 200,
+      result: true,
+      data: {
+        ...userFounded,
+        token
+      }
+    })
+  } else {
+    responseGenerator(res, {
+      url: req.url,
+      status: 403,
+      result: false,
+      data: {
+        message: 'Erro, dados inválidos'
+      }
+    })
+  }
 }
 
 export const logoutUser = (req: Request, res: Response) => {
-
+  responseGenerator(res, {
+    url: req.url,
+    status: 200,
+    result: true,
+    data: {
+      message: 'Logout user'
+    }
+  })
 }
