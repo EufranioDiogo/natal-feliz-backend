d3f957a5e26f201416bb0fcd632866731a071502
EufranioDiogo <eufraniodiogo5@gmail.com>
2021-12-15 13:52:01 +0100

    refactor: deleting dist

--- dist/config/apiConfig.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.DB_DEV_URL = exports.DB_PROD_URL = void 0;
-exports.DB_PROD_URL = 'mongodb://app_consumer:app_consumer_a@cluster0-shard-00-00.dfuhn.mongodb.net:27017,cluster0-shard-00-01.dfuhn.mongodb.net:27017,cluster0-shard-00-02.dfuhn.mongodb.net:27017/natal_feliz?ssl=true&replicaSet=atlas-n0wvkm-shard-0&authSource=admin&retryWrites=true&w=majority';
-exports.DB_DEV_URL = 'mongodb://localhost:27017/natal_feliz';
